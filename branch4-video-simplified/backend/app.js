b01c6e1cf3c6e615eda12b5799f4367e25805cc4
Sandip Ranjit <sandip.ranjit@metropolia.fi>
2025-12-03 11:49:33 +0200

    [Phase2] User auth and managing users

---
+++ branch4-video-simplified/backend/app.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+const express = require("express");
+const app = express();
+const userRouter = require("./routes/userRouter");
+const { unknownEndpoint,errorHandler } = require("./middleware/customMiddleware");
+const connectDB = require("./config/db");
+const cors = require("cors");
+
+// Middlewares
+app.use(cors())
+app.use(express.json());
+
+connectDB(); 
+
+// Use the jobRouter for all "/jobs" routes
+app.use("/api/users", userRouter);
+
+app.use(unknownEndpoint);
+app.use(errorHandler);
+
+module.exports = app;

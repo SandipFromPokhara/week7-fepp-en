bd61cac4bf9a1c4b4c01f6cf653dff95ecdbe5ad
Taysa Abinader <ferreira.taysa@gmail.com>
2025-12-03 13:42:48 +0200

    [Phase 3] Implementing Protected Routes

---
+++ branch7-protect-jobs/backend/routes/productRouter.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const { getAllProducts, createProduct, getProductById, updateProduct, deleteProduct } = require("../controllers/productControllers");
+const requireAuth = require('../middleware/requireAuth');
+
+const router = express.Router();
+
+router.get("/", getAllProducts);
+router.get("/:productId", getProductById);
+
+router.post("/", requireAuth, createProduct);
+router.put("/:productId", requireAuth, updateProduct);
+router.delete("/:productId", requireAuth, deleteProduct);
+
+module.exports = router;

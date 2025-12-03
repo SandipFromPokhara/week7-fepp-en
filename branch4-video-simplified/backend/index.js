8a59ec5a0d51a95006e29cecb796b3fb395c4d6e
Sandip Ranjit <sandip.ranjit@metropolia.fi>
2025-12-03 14:25:49 +0200

    Removee unwanted folders and clear up branch

--- branch4-video-simplified/backend/index.js
+++
@@ -1,8 +0,0 @@
-const app = require("./app");
-const http = require("http");
-
-const server = http.createServer(app);
-
-server.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`)
-})  

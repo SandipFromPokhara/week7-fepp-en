a455195a54eac83cceceff818665fd08e1e67287
Sandip Ranjit <sandip.ranjit@metropolia.fi>
2025-12-03 11:56:28 +0200

    Add routes, controller and model for products

--- branch4-video-simplified/backend/app.js
+++ branch4-video-simplified/backend/app.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require("express");
 const app = express();
 const userRouter = require("./routes/userRouter");
+const productRouter = require("./routes/productRouter");
 const { unknownEndpoint,errorHandler } = require("./middleware/customMiddleware");
 const connectDB = require("./config/db");
 const cors = require("cors");
@@ -12,7 +13,7 @@ app.use(express.json());
 
 connectDB(); 
 
-// Use the jobRouter for all "/jobs" routes
+app.use("/api/products", productRouter);
 app.use("/api/users", userRouter);
 
 app.use(unknownEndpoint);
